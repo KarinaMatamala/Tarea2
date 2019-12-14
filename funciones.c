87d00337cdf9fda06e5bd3ee3ec45070c475d43e
KarinaMatamala <52213006+KarinaMatamala@users.noreply.github.com>
2019-12-13 22:25:16 -0300

    Funciones agregar canciones y buscar cancion

---
+++ funciones.c
@@ -0,0 +1,35 @@
+/=====FUNCION *agregarCancion=====
+*/
+void agregarCancion(Map *mapaCanciones, Map *mapaAlbum, Map *mapaArtista){
+    char *nombre;
+    char *artista;
+    char *duracion;
+    char *album = 'unknow';
+
+    printf("Ingrese el nombre de la cancion, junto con su  artista y duracion todo separado por comas\n");
+    scanf("%[,]s",nombre);
+    scanf("%[,]s",artista);
+    scanf("%[,]s",duracion);
+
+
+    cancion *nueva = crear_cancion(nombre,artista,duracion,album);
+    if(searchMap(mapaCanciones,nombre)==NULL){
+        insertMap(mapaCanciones,nombre,nueva);
+    }
+    printf("\n Cancion agregada con exito c:\n");
+}
+/=====FUNCION *buscarCancion=====
+*/
+void buscarCancion(Map *mapaCanciones){
+    char *nombre;
+
+    printf("¿Que cancion desea buscar ?\n");
+    scanf("%s",nombre);
+    cancion *mostrar;
+    mostrar=searchMap(mapaCanciones,nombre);
+    if(mostrar==NULL){
+        printf("La cancion ingresada no se encontro \n");
+    }
+    else{
+        printf("Nombre: %s\nArtista:%s\nAlbum:%sDuracion:%s\n", mostrar->nombre, mostrar->artista, mostrar->album, mostrar->duracion);
+    }
